be7d52ffdf843f11f78921474b394cb82949ca03
karansurjith <karansurjith450@gmail.com>
2026-03-02 10:52:35 +0530

    added src

---
+++ src/OOPSBannerApp.java
@@ -0,0 +1,22 @@
+/**
+ * OOPSBannerApp
+ * 
+ * UC1: Prints the literal text "OOPS" to the console.
+ * This is the foundational step for the OOPS Banner App project.
+ * 
+ * @author karansurjith
+ * @version 1.0
+ */
+
+public class OOPSBannerApp {
+
+    /**
+     * Main method - entry point of the application.
+     * JVM starts execution from here.
+     * 
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
