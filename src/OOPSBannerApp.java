d62bd8e2d63f3695c9ea7280b6bc459f92ffd6a7
karansurjith <karansurjith450@gmail.com>
2026-03-02 11:44:50 +0530

    added

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -29,3 +29,65 @@ public class OOPSBannerApp {
         }
     }
 }
+public class OOPSBannerUC6 {
+
+    // Helper method to generate pattern for 'O'
+    public static String[] getOPattern() {
+        return new String[]{
+                String.join("", "  *****  "),
+                String.join("", " *     * "),
+                String.join("", "*       *"),
+                String.join("", "*       *"),
+                String.join("", "*       *"),
+                String.join("", " *     * "),
+                String.join("", "  *****  ")
+        };
+    }
+
+    // Helper method to generate pattern for 'P'
+    public static String[] getPPattern() {
+        return new String[]{
+                String.join("", "*******  "),
+                String.join("", "*      * "),
+                String.join("", "*      * "),
+                String.join("", "*******  "),
+                String.join("", "*        "),
+                String.join("", "*        "),
+                String.join("", "*        ")
+        };
+    }
+
+    // Helper method to generate pattern for 'S'
+    public static String[] getSPattern() {
+        return new String[]{
+                String.join("", " ******  "),
+                String.join("", "*        "),
+                String.join("", "*        "),
+                String.join("", " *****   "),
+                String.join("", "      *  "),
+                String.join("", "      *  "),
+                String.join("", " ******  ")
+        };
+    }
+
+    public static void main(String[] args) {
+
+        // Populate banner using methods
+        String[] o1 = getOPattern();
+        String[] o2 = getOPattern();
+        String[] p = getPPattern();
+        String[] s = getSPattern();
+
+        // Assemble banner
+        String[] banner = new String[7];
+
+        for (int i = 0; i < 7; i++) {
+            banner[i] = o1[i] + " " + o2[i] + " " + p[i] + " " + s[i];
+        }
+
+        // Enhanced for loop to print banner
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
